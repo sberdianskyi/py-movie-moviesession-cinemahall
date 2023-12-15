1ae8c6457fc0291d5cea9b183e8d4c3b5fa4a0cc
Serhii Berdianskyi <zeferot94@gmail.com>
2023-12-15 14:08:39 +0200

    Update

--- services/movie.py
+++ services/movie.py
@@ -1,10 +1,10 @@
-from typing import Any
+from django.db.models import QuerySet
 
 from db.models import Movie
 
 
 def get_movies(genres_ids: list[int] = None,
-               actors_ids: list[int] = None) -> Any:
+               actors_ids: list[int] = None) -> QuerySet:
     movies = Movie.objects.all()
 
     if genres_ids:
@@ -16,14 +16,14 @@ def get_movies(genres_ids: list[int] = None,
     return movies
 
 
-def get_movie_by_id(movie_id: int) -> Any:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
 def create_movie(movie_title: str,
                  movie_description: str,
                  genres_ids: list[int] = None,
-                 actors_ids: list[int] = None) -> Any:
+                 actors_ids: list[int] = None) -> None:
     new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description
